28a6c363af65eddf67f624c3916c329e2b042ade
Matthew Go <lezgomatt@gmail.com>
2025-07-26 23:48:22 +0800

    cleanup: More tweaks

--- hoy/hoy.py
+++ hoy/hoy.py
@@ -3,12 +3,12 @@ import subprocess
 import sys
 
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 
 def main() -> None:
     title = "Hoy!"
-    message = sys.argv[1] if len(sys.argv) > 1 else "Task completed."
+    message = " ".join(sys.argv[1:]) if len(sys.argv) > 1 else "Task completed."
 
     system = platform.system()
     if system == "Darwin":
